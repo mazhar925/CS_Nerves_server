f8d966af40c5b6de241a684c2c6f588fb020102f
mazhar925 <imazharul1101@gmail.com>
2022-10-27 23:36:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const courses = require('./data/courses.json');
+const coursesDetails = require('./data/details.json');
+const cors = require('cors');
+
+const port = 5000;
+
+app.use(cors());
+
+app.get('/courses', (req, res)=>{
+    res.send(courses);
+}
+
+)
+app.get('/details/:id', (req, res)=>{
+    const id = req.params.id;
+    const course = coursesDetails.find(n => n.id === id)
+    res.send(course)
+
+})
+
+app.listen(port)
